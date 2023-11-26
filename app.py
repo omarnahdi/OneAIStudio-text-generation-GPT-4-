37616b4a70d3b2ed65a568da34eae215284b6d8a
Omar Nahdi <136505046+omarnahdi@users.noreply.github.com>
2023-11-26 18:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-# # Edit this One AI API call using our studio at https://studio.oneai.com/?pipeline=llu0ZY&share=true
+ # Edit this One AI API call using our studio at https://studio.oneai.com/?pipeline=llu0ZY&share=true
 
-# import requests
-# api_key = "2120de50-8e64-4b34-95c5-00d1cb206cea"
-# url = "https://api.oneai.com/api/v0/pipeline"
-# user_input = input("Write a prompt: ")
-# headers = {
-#   "api-key": api_key, 
-#   "content-type": "application/json"
-# }
-# payload = {
-#   "input": user_input,
-#   "input_type": "article",
-# 	"output_type": "json",
-#   "multilingual": {
-#     "enabled": True
-#   },
-#   "steps": [
-#     {
-#       "skill": "gpt"
-#     }
-#   ],
-# }
+ import requests
+ api_key = 
+ url = "https://api.oneai.com/api/v0/pipeline"
+ user_input = input("Write a prompt: ")
+ headers = {
+   "api-key": api_key, 
+   "content-type": "application/json"
+ }
+ payload = {
+   "input": user_input,
+   "input_type": "article",
+ 	"output_type": "json",
+   "multilingual": {
+     "enabled": True
+   },
+   "steps": [
+     {
+       "skill": "gpt"
+     }
+   ],
+ }
 
 # r = requests.post(url, json=payload, headers=headers)
 # data = r.json()
